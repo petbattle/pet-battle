d1e06228a85ee144aa614ea3ef809659f6a592f3
donal <donalspring@gmail.com>
2020-04-04 00:56:12 +0100

    ADD service provider for configmap 🐙

--- src/app/env/env.service.provider.ts
+++ src/app/env/env.service.provider.ts
@@ -6,14 +6,14 @@ export const EnvServiceFactory = () => {
 
   // Read environment variables from browser window
   const browserWindow = window || {};
-  const browserWindowEnv = browserWindow['__env'] || {};
+  const browserWindowEnv = browserWindow.__env || {};
 
   // Assign environment variables from browser window to env
   // In the current implementation, properties from env.js overwrite defaults from the EnvService.
   // If needed, a deep merge can be performed here to merge properties instead of overwriting them.
   for (const key in browserWindowEnv) {
     if (browserWindowEnv.hasOwnProperty(key)) {
-      env[key] = window['__env'][key];
+      env[key] = window.__env[key];
     }
   }
 
