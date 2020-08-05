cf6195752af3dd58794bff060f4935ef3c8b3184
Mike Hepburn <eformat@gmail.com>
2020-08-05 15:40:50 +1000

    public flow, with pkce

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private oauthService: OAuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (this.oauthService.hasValidAccessToken()) {
+      var base64Url = this.oauthService.getAccessToken().split('.')[1];
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      var jsonPayload = decodeURIComponent(
+        atob(base64)
+          .split('')
+          .map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+          })
+          .join('')
+      );
+
+      console.log('jsonToken: ', JSON.parse(jsonPayload));
+      return true;
+    }
+
+    this.router.navigate(['']);
+    return false;
+  }
+}
