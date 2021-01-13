58e0c7deedca63e2150eafeef935f4fe46843bc9
donal <donalspring@gmail.com>
2021-01-13 18:03:46 +0000

    💥 Config load correction and sso stuff oh and configmap thingy 🐍

--- src/app/tournament/tournament-routing.moudule.ts
+++ src/app/tournament/tournament-routing.moudule.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TournamentComponent } from './tournament.component';
+import { AuthGuard } from '@app/auth/auth.guard';
 
 const routes: Routes = [
   // Module is lazy loaded, see app-routing.module.ts
-  { path: '', component: TournamentComponent, data: { title: 'Tournament' } }
+  {
+    path: '',
+    component: TournamentComponent,
+    data: { title: 'Tournament', roles: ['pbplayer'] },
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
--- src/app/tournament/tournament.component.ts
+++ src/app/tournament/tournament.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
-import { EnvService } from '@app/env/env.service';
-import { map, catchError } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
+import { ConfigurationLoader } from '@app/config/configuration-loader.service';
 
 @Component({
   selector: 'app-tournament',
@@ -11,10 +9,10 @@ import { Observable, of } from 'rxjs';
   styleUrls: ['./tournament.component.scss']
 })
 export class TournamentComponent implements OnInit {
-  private apiUrl: string;
+  private tournamentUrl: string;
 
-  constructor(private httpClient: HttpClient, private env: EnvService) {
-    this.apiUrl = this.env.customEnv.apiUrl;
+  constructor(private httpClient: HttpClient, private configSvc: ConfigurationLoader) {
+    this.tournamentUrl = this.configSvc.getConfiguration().tournaments;
   }
 
   ngOnInit() {}
@@ -23,7 +21,7 @@ export class TournamentComponent implements OnInit {
     console.log('>>> tournament: ');
     this.httpClient
       .cache()
-      .get(`${this.apiUrl}/api/tournament/348f6ae0-109f-4a3c-886d-2c2618e74246/leaderboard`)
+      .get(`${this.tournamentUrl}/api/tournament/348f6ae0-109f-4a3c-886d-2c2618e74246/leaderboard`)
       .subscribe(payload => console.log(payload));
   }
 }
