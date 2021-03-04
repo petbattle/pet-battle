c95d85ab9296aabe190e5ff50af4b6d5ebc7aeb0
donal <donalspring@gmail.com>
2021-03-04 15:13:34 +0000

    🐻 upgrate to angular 11 🐻

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,16 @@ import { Shell } from '@app/shell/shell.service';
 
 const routes: Routes = [
   Shell.childRoutes([
-    { path: 'winner', loadChildren: './winner/winner.module#WinnerModule' },
-    { path: 'about', loadChildren: './about/about.module#AboutModule' },
-    { path: 'tournament', loadChildren: './tournament/tournament.module#TournamentModule' }
+    { path: 'winner', loadChildren: () => import('./winner/winner.module').then(m => m.WinnerModule) },
+    { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+    { path: 'tournament', loadChildren: () => import('./tournament/tournament.module').then(m => m.TournamentModule) }
   ]),
   // Fallback when no prior route is matched
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
   providers: []
 })
